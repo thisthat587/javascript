e62319e2d2cfe358cbf048b62b5efe62cdb6f8c4
thisthat587 <thisthat587@gmail.com>
2023-11-20 15:26:42 +0530

    for(in,of)

---
+++ for(of,in).js
@@ -0,0 +1,32 @@
+console.clear();
+
+const string = ['Piyush', 'Anand', 'Pankaj', 'Ashish', 'Badal'];
+
+console.log("\nOF\n")
+console.log("-------------------------");
+// It will print the value in the array...
+// for(of) prints direct value of an iterable data structure...
+for (value of string) {
+    console.log(value)
+}
+
+console.log("-------------------------");
+console.log("\nIN\n");
+// It will print index in the array...
+// for(IN) is basically used to print key or the value associated to the key
+
+for (key in string) {
+    console.log(key);
+}
+// It will print the value in the array...
+for (key in string) {
+    console.log(string[key]);
+}
+
+const object={
+    name:"Piyush",
+    hobby:"coding",
+    address:"patna",
+    surname:"singh"
+
+}
