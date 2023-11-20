b8fda4d2e9947c3d0973148d487674f344082750
thisthat587 <thisthat587@gmail.com>
2023-11-20 17:06:42 +0530

    aivayi

---
+++ js_set.js
@@ -0,0 +1,5 @@
+//Only unique values can be stored, if same values are stored it will not be accesible
+
+const new_set =new Set(['a','m','e']);
+console.log(new_set.values())
+// console.log(new_set[1]);
