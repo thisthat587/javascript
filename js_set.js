240613d042037d24017d89a31f93c51695cd72f3
thisthat587 <thisthat587@gmail.com>
2023-11-21 11:29:34 +0530

    set in javascript

--- js_set.js
+++ js_set.js
@@ -1,5 +1,12 @@
 //Only unique values can be stored, if same values are stored it will not be accesible
 
-const new_set =new Set(['a','m','e']);
-console.log(new_set.values())
+let name="piyush";
+name[0]="h";
+console.log(name);
+// const new_set =new Set(['a','m','e']);
+// for(value of new_set.values()){
+//     console.log(value)
+// }
+// console.log(new_set.size)
+// console.log(new_set.values())
 // console.log(new_set[1]);
