880cc7cd476fbe6ddb707498dd1e2980d08b2441
thisthat587 <thisthat587@gmail.com>
2023-11-25 20:58:48 +0530

    commit

--- js_Promises.js
+++ js_Promises.js
@@ -1,23 +1,42 @@
-let promise = new Promise((resolve, reject) => {
+// let promise = new Promise((resolve, reject) => {
 
-    x = 0;
-    if (x === 0) {
-        resolve("SAME");
-    }
-    else {
-        reject("Error Occured...!");
-    }
-})
+//     x = 0;
+//     if (x === 0) {
+//         resolve("SAME");
+//     }
+//     else {
+//         reject("Error Occured...!");
+//     }
+// })
 
-promise.then((value) => {
-    console.log(value);
-    return "OK";
-})
-    .then((value) => {
-        console.log(value);
-    })
-    .catch((error) => {
-        console.log(error);
-    }).finally(() => {
-        console.log("It Ends.......");
-    })
+// promise.then((value) => {
+//     console.log(value);
+//     return "OK";
+// })
+//     .then((value) => {
+//         console.log(value);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     }).finally(() => {
+//         console.log("It Ends.......");
+//     })
+
+
+function myDisplayer(){
+    console.log("Hello world...!");
+}
+function getFile(myCallback) {
+    let req = new XMLHttpRequest();
+    req.open('GET', "mycar.html");
+    req.onload = function() {
+      if (req.status == 200) {
+        myCallback(req.responseText);
+      } else {
+        myCallback("Error: " + req.status);
+      }
+    }
+    req.send();
+  }
+  
+  getFile(myDisplayer)
