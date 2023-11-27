3918731fd60b19d709a7f331ff452698ef83c52e
thisthat587 <thisthat587@gmail.com>
2023-11-27 09:56:04 +0530

    db

--- js_appIntoFunction.js
+++ js_appIntoFunction.js
@@ -1,36 +1,3 @@
-// const mysql = require('mysql2');
-// let data = [];
-// const connection = mysql.createConnection({
-//         host: '89.117.188.154',
-//         user: 'u932299896_eduware',
-//         password: 'Webgen@220310',
-//         database: 'u932299896_sisdb'
-// });
-
-// connection.connect(function result(error) {
-//         if (error) {
-//                 console.log("ERROR: Something Went Wrong...!\n\n", error);
-//         } else {
-//                 console.log("Connection done...");
-//                 const query = 'SELECT admno,name, fname, class, section, roll, fmob, session, active,transport FROM tbl_admission WHERE session = "2023-2024" AND active = 1';
-//                 getData(query);
-//         }
-// });
-
-// function getData(query) {
-//         connection.query(query, function (error, result) {
-//                 if (error) {
-//                         console.log("ERROR : Error executing query...!")
-//                 } else {
-//                         for (row of result) {
-//                                 data.push(row);
-//                                 // console.log(row.fmob);
-//                         }
-//                         connection.end();
-//                 }
-//                 console.log(data);
-//         })
-// }
 
 const { app, BrowserWindow } = require('electron');
 const { json } = require('express');
