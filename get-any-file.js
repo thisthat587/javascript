873dcf21bfa05242f224b0a4d9b455f4c268805e
thisthat587 <thisthat587@gmail.com>
2023-12-01 18:37:23 +0530

    ..........

---
+++ get-any-file.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+const folderPath = 'C:\\Users\\Piyush Singh\\Pictures';
+const fileName = 'Screenshot 2023-02-25 202500.png';
+
+const filePath = path.join(folderPath, fileName);
+
+console.log("File Path:", filePath);
